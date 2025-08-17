2b76fd330b3bdc6cfeb189802d49496da7e0b8ae
dipo_ubuntu <dipo_ubuntu@gmail.com>
2025-08-17 14:38:50 +0800

    fix testshell and update doit

--- content/shell/testshell.sh
+++ content/shell/testshell.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
-for n in {1..$RANDOM}
-do
+
+# 使用 /dev/urandom 生成随机数
+rand() {
+  # 取两个字节，转换成十进制
+  od -An -N2 -tu2 /dev/urandom
+}
+
+max=$(rand)
+
+n=1
+while [ "$n" -le "$max" ]; do
   str=""
-  if (( n % 3 == 0 ))
-  then
+
+  if [ $((n % 3)) -eq 0 ]; then
     str="fizz"
   fi
-  if [ $[n%5] == 0 ]
-  then
-    str="$strbuzz"
+
+  if [ $((n % 5)) -eq 0 ]; then
+    str="${str}buzz"
   fi
-  if [[ ! $str ]]
-  then
+
+  if [ -z "$str" ]; then
     str="$n"
   fi
+
   echo "$str"
+  n=$((n + 1))
 done
